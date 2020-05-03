6cbd6d3a008dafdcc22ddada3b2c6db6a3a80084
kvsrinivas91 <kvsrinivas91@gmail.com>
2020-05-03 15:20:45 +0530

    Changed distance

---
+++ MOTOR_ULTRASONIC/MOTOR_ULTRASONIC.ino
@@ -0,0 +1,44 @@
+int trigPin=4;
+int echoPin=5;
+long duration;
+int distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  delay(2);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  delay(2);
+  digitalWrite(trigPin,LOW);
+  delay(2);
+  digitalWrite(trigPin,HIGH);
+  delay(2);
+  digitalWrite(trigPin,LOW);
+duration=pulseIn(echoPin,HIGH); 
+distance=duration*0.034/2;
+Serial.println(distance);
+if (distance<=30)
+ {
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+delay(20);
+digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+  delay(20);
+ }
+}
